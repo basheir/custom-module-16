decc151846c74be9bc0db44600720b678717b5a7
Bashir Mahad <basheirmahad@gmail.com>
2024-11-04 17:23:37 +0300

    Hospital module added

---
+++ hospital_management/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Hospital Management system",
+    "license": "LGPL-3",
+    "version": "16.0.1.1",
+    "category": "sales",
+    "author": "Bashir Mahad",
+    "website": "course.haybis.com",
+    "installable": True,
+    "data":[
+        "views/menu.xml"
+    ]
+}
---
+++ hospital_management/models/patient.py
@@ -0,0 +1,10 @@
+from odoo import  api, fields, models
+
+
+class HospitalPatient(models.Model):
+    _name = "hospital.patient"
+    _description = "Patient master"
+
+    name = fields.Char(string="Name")
+    date_of_birth =fields.Date(string="DOB")
+    gender = fields.Selection([("male", "Male"), ("female", "Female")])
