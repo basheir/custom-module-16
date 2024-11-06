19d64d6525a227c24f4c5f10da031de954f9b941
Bashir Mahad <basheirmahad@gmail.com>
2024-11-06 06:46:45 +0300

    Creating Views And Tracking In Odoo

--- hospital_management/models/patient.py
+++ hospital_management/models/patient.py
@@ -8,3 +8,4 @@ class HospitalPatient(models.Model):
     name = fields.Char(string="Name")
     date_of_birth =fields.Date(string="DOB")
     gender = fields.Selection([("male", "Male"), ("female", "Female")])
+    phone = fields.Integer(string="Phone Number")
