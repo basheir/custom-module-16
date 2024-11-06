a3e2aabc07b61a30b8c9a6f40af86bd8ccd85c15
Bashir Mahad <basheirmahad@gmail.com>
2024-11-06 06:59:08 +0300

    Optional hide/show columns added

--- hospital_management/models/patient.py
+++ hospital_management/models/patient.py
@@ -5,7 +5,7 @@ class HospitalPatient(models.Model):
     _name = "hospital.patient"
     _description = "Patient master"
 
-    name = fields.Char(string="Name")
+    name = fields.Char(string="Name", required=True)
     date_of_birth =fields.Date(string="DOB")
     gender = fields.Selection([("male", "Male"), ("female", "Female")])
     phone = fields.Integer(string="Phone Number")
